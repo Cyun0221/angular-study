e1dfa0b1e7607b104bfcb8eb34a7d44e468e0437
Cyun0221 <913645371@qq.com>
2021-02-18 23:30:57 +0800

    改变展示形式

--- src/app/component/s1-position/s1-position.component.ts
+++ src/app/component/s1-position/s1-position.component.ts
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./s1-position.component.css']
 })
 export class S1PositionComponent implements OnInit {
+  displayStatic = false;
+  displayAbsolute = false;
+  displayRelative = false;
+  displayFixed = false;
+  displaySticky = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  display(e: number): void {
+    this.displayStatic = false;
+    this.displayAbsolute = false;
+    this.displayRelative = false;
+    this.displayFixed = false;
+    this.displaySticky = false;
+    switch (e) {
+      case 1:
+        this.displayStatic = !this.displayStatic;
+        break;
+      case 2:
+        this.displayAbsolute = !this.displayAbsolute;
+        break;
+      case 3:
+        this.displayRelative = !this.displayRelative;
+        break;
+      case 4:
+        this.displayFixed = !this.displayFixed;
+        break;
+      case 5:
+        this.displaySticky = !this.displaySticky;
+        break;
+      default:
+        this.displayStatic = false;
+        this.displayAbsolute = false;
+        this.displayRelative = false;
+        this.displayFixed = false;
+        this.displaySticky = false;
+    }
+  }
 }
